d1de443b4ce3f02d6db6ad3fba0032b2ca627bcf
zhengb <zhengb@guance.com>
2022-06-27 10:30:35 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zhengboah/gotest
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main () {
+  fmt.Println("this is test")
+}
+
+func Foo () {
+  fmt.Println("foo test")
+}
